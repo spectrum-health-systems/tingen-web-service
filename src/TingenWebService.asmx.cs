8e57c39442f281ae9733cde921146fe2f0a39526
Chris Banwarth <github@aprettycoolprogram.com>
2025-05-15 13:08:04 -0400

    25.5.0-development+250515

--- src/TingenWebService.asmx.cs
+++ src/TingenWebService.asmx.cs
@@ -18,13 +18,8 @@
 // u250515_code
 // u250515_documentation
 
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Reflection;
-using System.Security.Policy;
 using System.Web.Services;
-using System.Web.UI.WebControls.WebParts;
 using Outpost31.Core.Logger;
 using Outpost31.Core.Session;
 using ScriptLinkStandard.Objects;
@@ -50,7 +45,7 @@ namespace TingenWebService
     ///         While you're here, take a look at this stuff:
     ///         <list type="bullet">
     ///             <item>The <see href="https://github.com/spectrum-health-systems/Tingen-Documentation">Tingen Documentation</see></item>
-    ///             <item>The<see href = "https://github.com/spectrum-health-systems/Tingen-Documentation/tree/main/docs">Tingen Web Service API Documentation</see></item>
+    ///             <item>The <see href="https://github.com/spectrum-health-systems/Tingen-Documentation/tree/main/docs">Tingen Web Service API Documentation</see></item>
     ///         </list>
     ///     </para>
     /// </remarks>
@@ -64,29 +59,27 @@ namespace TingenWebService
         /// <remarks>A required component for writing log files, defined here so it can be used throughout the class.</remarks>
         public static string ExeAsm { get; set; } = Assembly.GetExecutingAssembly().GetName().Name;
 
-        /// <summary>The Tingen current version number.</summary>
+        /// <summary>The current version of the Tingen Web Service.</summary>
         /// <remarks>The version number is pulled from <i>Properties/AssemblyInfo.cs</i></remarks>
         /// <value>YY.MM.Patch(e.g., <c>25.02.1</c>)</value>
         /// <seealso href="https://github.com/spectrum-health-systems/Tingen-Documentation/blob/main/Static/TngnWbsv-Versioning.md">More information about versions.</seealso>
         public static string TngnWbsvVersion { get; set; } = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         /// <summary>The environment that the Tingen Web Service will interface with.</summary>
-        /// <remarks>This needs to be.</remarks>
-        //public static string TngnWbsvEnvironment { get; set; } = "UAT";
+        /// <remarks>This needs to be manually updated to "LIVE" when deploying to production.</remarks>
+        /// <value><c>UAT</c> (testing) or <c>LIVE</c>(production)</value>
+        public static string TngnWbsvEnvironment { get; set; } = "UAT";
 
         /// <summary>Get the current version of Tingen.</summary>
-        /// <returns>The current version number of Tingen.</returns>
-        /// <include file='AppData/XmlDoc/TingenWebService.xml' path='TingenWebService/Class[@name="TingenWebService"]/GetVersion/*'/>
-        [WebMethod]
-        public string GetVersion() => $"VERSION {TngnWbsvVersion}";
-
-        /// <summary>The entry point for the Tingen Web Service.</summary>
         /// <remarks>
         ///     <note title="About this method">
         ///         <list type="bullet">
-        ///             <item>This method is required by Avatar and <i>should not be modified</i>.</item>
-        ///             <item>Trace Logs can't go here because the logging infrastructure<br/>
-        ///                   hasn't been initialized yet.
+        ///             <item>
+        ///                 This method is required by Avatar and <i>should not be modified</i>.
+        ///             </item>
+        ///             <item>
+        ///                 Trace Logs can't go here because the logging infrastructure<br/>
+        ///                 hasn't been initialized yet.
         ///             </item>
         ///         </list>
         ///     </note>
@@ -94,27 +87,41 @@ namespace TingenWebService
         ///         For more information on the Tingen Web Service version<br/>
         ///         number, please see the<see cref="TngnWbsvVersion">TngnVersion</see> property.
         ///     </para>
-        ///     This method logs critical events if the input parameters are invalid and debug-level
-        ///     events during the script execution process. Ensure that both <paramref name="sentOptObj"/> and <paramref
-        ///     name="sentSlnkScriptParam"/> are valid before calling this method.
+        ///    </remarks>
+        /// <returns>The current version number of Tingen.</returns>
+        [WebMethod]
+        public string GetVersion() => $"VERSION {TngnWbsvVersion}";
+
+        /// <summary>The entry point for the Tingen Web Service.</summary>
+        /// <remarks>
+        ///     <note title="About this method">
+        ///         <list type="bullet">
+        ///             <item>
+        ///                 This method is required by Avatar and <i>should not be modified</i>.
+        ///             </item>
+        ///             <item>Trace Logs can't go here because the logging infrastructure<br/>
+        ///                   hasn't been initialized yet.
+        ///             </item>
+        ///         </list>
+        ///     </note>
+        ///     <note type="security" title="Critical failure warning">
+        ///         If Avatar doesn't pass an <paramref name="sentOptObj"/> or <paramref name="sentSlnkScriptParam"/>, the Tingen Web Service will fail.<br/>
+        ///         <br/>
+        ///         Since this can be difficult to troubleshoot, a<c> Critical Failure Log</c> will be created.<br/>
+        ///         <br/>
+        ///         Generally the fix for a missing OptionObject and/or Script Parameter is to<br/>
+        ///         re-import the Tingen Web Service WSDL.
+        ///     </note>
         /// </remarks>
-        /// <param name="sentOptObj">The <see cref="OptionObject2015"/> instance containing the data to be processed by the script.</param>
-        /// <param name="sentSlnkScriptParam">A string representing the script parameter to be used during execution. Cannot be null, empty, or
-        /// whitespace.</param>
-        /// <returns>An <see cref="OptionObject2015"/> instance representing the result of the script execution.  If the input
-        /// parameters are invalid, a default <see cref="OptionObject2015"/> with no changes is returned.</returns>
+        /// <param name="sentOptObj">The <see cref="OptionObject2015"/> sent from Avatar, which contains all of the content and metadata of an Avatar form.</param>
+        /// <param name="sentSlnkScriptParam">The Script Parameter that is sent from Avatar, which contains the request the Tingen Web Service needs to do work.</param>
+        /// <returns>An <see cref="OptionObject2015"/> representing the result of the Script Parameter request.</returns>
         [WebMethod]
         public OptionObject2015 RunScript(OptionObject2015 sentOptObj, string sentSlnkScriptParam)
         {
-            var tngnWbsvEnvironment = File.ReadAllText(@"..\AppData\Runtime\TngnWbsv.environment").Trim();
-
-            /* Uncomment the following line to completely disable the Tingen Web Service.
-             */
-            //return sentOptObj.ToReturnOptionObject(0, "");
-
             if (string.IsNullOrWhiteSpace(sentSlnkScriptParam) || sentOptObj == null)
             {
-                LogEvent.Critical(tngnWbsvEnvironment, Outpost31.Core.Template.Message.TngnWbsvCriticalMissingArguments(sentOptObj, sentSlnkScriptParam));
+                LogEvent.Critical(TngnWbsvEnvironment, Outpost31.Core.Template.Message.TngnWbsvCriticalMissingArguments(sentOptObj, sentSlnkScriptParam));
 
                 /* This really should just be a stop - can't return something that doesn't exist.
                  */
@@ -122,23 +129,15 @@ namespace TingenWebService
             }
             else
             {
-                /* Depreciated [250515]
-                 */
-                //var tngnWbsvSession = new TngnWbsvSession();
+                var tngnWbsvSession = TngnWbsvSession.New(sentOptObj, sentSlnkScriptParam, TngnWbsvVersion, TngnWbsvEnvironment);
 
-                var tngnWbsvSession = TngnWbsvSession.New(sentOptObj, sentSlnkScriptParam, TngnWbsvVersion, tngnWbsvEnvironment);
+                LogEvent.Debuggler(TngnWbsvEnvironment, "[SPIN UP]");
 
-                LogEvent.Debuggler(tngnWbsvEnvironment, "[SPIN UP]");
-
-                /* Most likely not needed [250515]
-                 */
-                //Spin.Up(tngnWbsvSession, sentOptObj, sentSlnkScriptParam, TngnWbsvVersion, TngnWbsvEnvironment);
-
-                LogEvent.Debuggler(tngnWbsvEnvironment, $"[PARSE REQUEST] '{tngnWbsvSession.SentScriptParam}'");
+                LogEvent.Debuggler(TngnWbsvEnvironment, $"[PARSE REQUEST] '{tngnWbsvSession.SentScriptParam}'");
 
                 Outpost31.Core.Avatar.ScriptParameter.Request(tngnWbsvSession);
 
-                LogEvent.Debuggler(tngnWbsvEnvironment, "[COMPLETE]");
+                LogEvent.Debuggler(TngnWbsvEnvironment, "[COMPLETE]");
 
                 return tngnWbsvSession.ReturnOptObj;
             }
