895379feb64397e0695b7010e222816179685377
Chris Banwarth <github@aprettycoolprogram.com>
2025-08-04 10:10:25 -0400

    25.8-development+250804
    Update documentation

--- src/ProjectInfo.cs
+++ src/ProjectInfo.cs
@@ -1,5 +1,5 @@
 ﻿/* Tingen Web Service
- * A custom web service for Netsmart's Avatar EHR
+ * A custom web service for Netsmart's Avatar EHR.
  * Release 25.8
  *
  * https://github.com/spectrum-health-systems/tingen-web-service
@@ -13,23 +13,13 @@
 namespace TingenWebService
 {
     ///<summary>Provides additional information about the Tingen Web Service.</summary>
-    ///<remarks>See the <see cref="ProjectInfo"/> class for more information.</remarks>
+    ///<remarks>
+    ///     <include file='AppData/XmlDoc/TingenWebService.xml' path='TingenWebService/Class[@name="Common"]/About/*'/><br/>
+    ///     <br/>
+    ///     <include file='AppData/XmlDoc/TingenWebService.xml' path='TingenWebService/Class[@name="Common"]/Documentation/*'/>
+    ///</remarks>
     internal class ProjectInfo
     {
-        // This class provides additional information about the Tingen Web Service.
-        // This class does not contain any executable code.
-
-        // About the Tingen Web Service
-        // ----------------------------
-        // Netsmart's Avatar™ is a behavioral health EHR that offers a recovery-focused suite of solutions that leverage real-time analytics
-        // and clinical decision support to drive value-based care.
-        //
-        // While Avatar™ is a robust platform, it isn't perfect. The good news is that you can extend myAvatar™ functionality via Netsmart's
-        // Avatar™ Web Services, and/or custom web services that are written by other Avatar™ users.
-        //
-        // Tingen is one such custom web service which includes various tools and utilities for Avatar™ that aren't included in the official
-        // release, and provides a solid foundation for building additional functionality quickly and efficiently.
-
-
+        // This class does not contain and executable code.
     }
 }
