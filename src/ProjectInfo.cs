8bb0cb210d4da46b6daab04c5a482544b014242d
Chris Banwarth <github@aprettycoolprogram.com>
2025-08-04 09:52:40 -0400

    Update ProjectInfo.cs

--- src/ProjectInfo.cs
+++ src/ProjectInfo.cs
@@ -23,11 +23,13 @@ namespace TingenWebService
         // ----------------------------
         // Netsmart's Avatar™ is a behavioral health EHR that offers a recovery-focused suite of solutions that leverage real-time analytics
         // and clinical decision support to drive value-based care.
-
+        //
+        // While Avatar™ is a robust platform, it isn't perfect. The good news is that you can extend myAvatar™ functionality via Netsmart's
+        // Avatar™ Web Services, and/or custom web services that are written by other Avatar™ users.
+        //
         // Tingen is one such custom web service which includes various tools and utilities for Avatar™ that aren't included in the official
         // release, and provides a solid foundation for building additional functionality quickly and efficiently.
 
-        // Netsmart's Avatar™ is a behavioral health EHR that offers a recovery-focused suite of solutions that leverage real-time analytics and clinical decision support to drive value-based care.
 
     }
 }
